34f6b803e037117096554b6fb89c01d807ce04f0
konetichandrakant <konetichandrakant@gmail.com>
2025-11-24 00:07:30 -0500

    fix: updated slow_mo functionality to use global env value

--- tests/test_login_task.py
+++ tests/test_login_task.py
@@ -1,5 +1,5 @@
 from src.tasks.login_task import Login
-from src.config import WEBSITE_URL, USER_CREDENTIALS, BROWSER_HEADLESS
+from src.config import WEBSITE_URL, USER_CREDENTIALS, BROWSER_HEADLESS, BROWSER_SLOW_MO
 import pytest
 
 @pytest.mark.asyncio
@@ -8,7 +8,7 @@ async def test_login():
         username, password = USER_CREDENTIALS
         
         # Run the login function
-        login_task = Login(website_url=WEBSITE_URL, username=username, password=password, headless=BROWSER_HEADLESS)
+        login_task = Login(website_url=WEBSITE_URL, username=username, password=password, headless=BROWSER_HEADLESS, slow_mo=BROWSER_SLOW_MO)
         await login_task.execute()
         
         # If no exceptions were raised, we assume the test passed
